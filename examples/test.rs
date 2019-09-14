76d355b3bf8ce411504186602388b2166f935917
Doug Lau <doug.p.lau@gmail.com>
2019-09-14 13:50:35 -0500

    Moved svg code into a separate module.

--- examples/test.rs
+++
@@ -1,7 +0,0 @@
-use score2svg::*;
-
-fn main() {
-    let string = test_svg(DEFAULT);
-
-    std::fs::write("test.svg", string).unwrap(); // _string
-}
--- src/lib.rs
+++ src/lib.rs
@@ -17,9 +17,12 @@
 //     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 mod glyph;
-pub use glyph::GlyphId;
+mod svg;
 
-use scof::{Cursor, Marking, Note, Scof, Fraction};
+pub use glyph::GlyphId;
+pub use svg::{Element, Group, Path, Rect, Use};
+
+use scof::{Cursor, Marking, Note, Scof};
 use std::fmt;
 
 /// Width of one bar (measure)
@@ -36,134 +39,6 @@ const WHOLE_REST_WIDTH: i32 = 230;
 /// Get Bravura font paths
 pub fn bravura() -> Vec<Path> {
     include!("vfont/bravura.vfont")
-}
-
-pub struct Rect {
-    pub x: i32,
-    pub y: i32,
-    pub width: u32,
-    pub height: u32,
-    pub rx: Option<u32>,
-    pub ry: Option<u32>,
-    pub fill: Option<String>,
-}
-
-impl fmt::Display for Rect {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "<rect x=\"{}\" y=\"{}\" width=\"{}\" height=\"{}\"",
-            self.x, self.y, self.width, self.height)?;
-        if let Some(ref rx) = self.rx {
-            write!(f, " rx=\"{}\"", rx)?;
-        }
-        if let Some(ref ry) = self.ry {
-            write!(f, " ry=\"{}\"", ry)?;
-        }
-        if let Some(ref fill) = self.fill {
-            write!(f, " fill=\"{}\"", fill)?;
-        }
-        write!(f, "/>")
-    }
-}
-
-impl Rect {
-    fn new(x: i32, y: i32, width: u32, height: u32, rx: Option<u32>,
-        ry: Option<u32>, fill: Option<u32>) -> Self
-    {
-        let fill = match fill {
-            Some(f) => Some(format!("#{:x}", f)),
-            None => None,
-        };
-        Rect { x, y, width, height, rx, ry, fill }
-    }
-}
-
-pub struct Use {
-    pub x: i32,
-    pub y: i32,
-    pub glyph: GlyphId,
-}
-
-impl fmt::Display for Use {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "<use x=\"{}\" y=\"{}\" xlink:href=\"#{:x}\"/>", self.x,
-            self.y, self.glyph as u32)
-    }
-}
-
-impl Use {
-    fn new(x: i32, y: i32, glyph: GlyphId) -> Self {
-        Use { x, y, glyph }
-    }
-}
-
-pub struct Group {
-    pub x: i32,
-    pub y: i32,
-    pub elements: Vec<Element>,
-}
-
-impl fmt::Display for Group {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "<g transform=\"translate({} {})\">", self.x, self.y)?;
-        for elem in &self.elements {
-            write!(f, "{}", elem)?;
-        }
-        write!(f, "</g>")
-    }
-}
-
-impl Group {
-    fn new(x: i32, y: i32) -> Self {
-        let elements = vec![];
-        Group { x, y, elements }
-    }
-    fn push(&mut self, elem: Element) {
-        self.elements.push(elem);
-    }
-}
-
-pub struct Path {
-    pub id: Option<String>,
-    pub d: String,
-}
-
-impl fmt::Display for Path {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if let Some(ref id) = self.id {
-            write!(f, "<path id=\"{}\" d=\"{}\"/>", id, self.d)
-        } else {
-            write!(f, "<path d=\"{}\"/>", self.d)
-        }
-    }
-}
-
-impl Path {
-    fn new<T: Into<String>>(id: Option<T>, d: T) -> Self {
-        let id = match id {
-            Some(id) => Some(id.into()),
-            None => None,
-        };
-        let d = d.into();
-        Path { id, d }
-    }
-}
-
-pub enum Element {
-    Group(Group),
-    Rect(Rect),
-    Use(Use),
-    Path(Path),
-}
-
-impl fmt::Display for Element {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Element::Group(g) => g.fmt(f),
-            Element::Rect(r) => r.fmt(f),
-            Element::Use(u) => u.fmt(f),
-            Element::Path(p) => p.fmt(f),
-        }
-    }
 }
 
 /// Staff lines
@@ -405,27 +280,7 @@ impl MeasureElem {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    #[test]
-    fn rect() {
-        assert_eq!(Rect::new(10, 12, 25, 20, None, None, None).to_string(),
-        "<rect x=\"10\" y=\"12\" width=\"25\" height=\"20\"/>");
-    }
-
-    #[test]
-    fn glyph() {
-        assert_eq!(Use::new(37, 21, GlyphId::StemHarpStringNoise).to_string(),
-        "<use x=\"37\" y=\"21\" xlink:href=\"#e21f\"/>");
-    }
-
-    #[test]
-    fn group() {
-        let mut group = Group::new();
-        group.push(Element::Use(Use::new(2, 3, GlyphId::NoteheadWhole)));
-        assert_eq!(group.to_string(),
-        "<g><use x=\"2\" y=\"3\" xlink:href=\"#e0a2\"/></g>");
-    }
+    // use super::*;
 
     // stamp(out, NoteheadFill, offset_x + 2000, STAFF_DY - STEP_DY * 3);
     // Clef
